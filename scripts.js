3e7ca734222db46166f10bcf0a8dae264777b573
snaeja <sea28@hi.is>
2024-10-17 15:35:05 +0000

    allt verkefnið leyst

--- scripts.js
+++ scripts.js
@@ -47,34 +47,148 @@ function split(str, separator = ' ') {
 
 //------------------------------------------------------------------------------
 // Grunnföll sem skilgreina á
-
+/**
+ * Fallið finnur og skilar lengsta orði í streng
+ * @param {string} str strengur af orðum sem skipt er niður með bilum.
+ * @returns {string} strengur sem er lengsta orðið í str. Ef fleiri en eitt orð
+ *  hafa lengstu lengd skilar fallið fyrsta orðinu sem kemur fyrir
+ */
 function longest(str) {
-  // Útfæra
+  let splitted = split(str);
+  let longest = "";
+  let max_length = 0;
+  for (let i = 0; i < splitted.length; i++) {
+      if (splitted[i].length > max_length) {
+        max_length = splitted[i].length;
+        longest = splitted[i];
+      }
+  }
+  return longest;
 }
 
+console.assert(longest("þetta er strengurinn minn") === "strengurinn", 'longest: skilar lengsta orðinu í streng');
+console.assert(longest("halló hafnarfjörður") === "hafnarfjörður", 'longest: skilar lengsta orðinu í streng');
+
+/**
+ * Fallið finnur og skilar stysta orði í streng
+ * @param {string} str strengur af orðum skipt niður með bilum.
+ * @returns {string} strengur sem er stysta orðið í str. Ef fleiri en einn sem eru af
+ * minnstu lengd skilar fallið fyrsta orðinu af stystu lengd
+ */
 function shortest(str) {
-  // Útfæra
+  let splitted = split(str);
+  let shortest = "";
+  let min_length = Infinity;
+  for (let i = 0; i < splitted.length; i++) {
+    if (splitted[i].length < min_length) {
+      min_length = splitted[i].length;
+      shortest = splitted[i];
+    }
+  }
+  return shortest;
 }
 
+console.assert(shortest("þetta er strengurinn minn") === "er", 'longest: skilar stysta orðinu í streng');
+console.assert(shortest("halló hafnarfjörður") === "halló", 'longest: skilar stysta orðinu í streng');
+
+/**
+ * Fallið skilar streng í öfugri röð
+ * @param {string} str strengur sem á að snúa við
+ * @returns {string} strengurinn str í öfugri röð
+ */
 function reverse(str) {
-  // Útfæra
+  let reversed = "";
+  for (let i = 0; i < str.length ; i++){
+    reversed += str[str.length - 1 - i];
+  }
+  return reversed;
 }
 
+console.assert(reverse("hallo") === "ollah", 'reverse: skrifar streng í öfugri röð');
+console.assert(reverse("búið") === "ðiúb", 'reverse: skrifar streng í öfugri röð');
+
+/**
+ * Fallið athugar hvort strengur sé palindrome þ.e. hvort hann sé eins þegar
+ * hann er lesinn afturábak og áfram.
+ * @param {string} str strengurinn sem á að athuga hvort sé palindrome
+ * @returns true ef str er palindrome strengur, annars false
+ */
 function palindrome(str) {
-  // Útfæra
+  if (str == reverse(str)) {
+    return true;
+  }
+  return false;
 }
 
+console.assert(palindrome("abba") === true, 'palindrome: athugar hvort strengur sé palindrome, ef svo er skilar true, annars false');
+console.assert(palindrome("búið") === false, 'palindrome: athugar hvort strengur sé palindrome, ef svo er skilar true, annars false');
+
+/**
+ * Fallið athugar hve margir sérljóðar úr íslenska stafrófinu eru í streng
+ * @param {string} str strengurinn sem á að finna fjölda sérhljóða í 
+ * @returns fjöldi sérhljóða í str
+ */
 function vowels(str) {
-  // Útfæra
+  let nmbr = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (VOWELS.includes(str[i])) {
+      nmbr += 1;
+    }
+  }
+  return nmbr;
 }
 
+console.assert(vowels("hallo") === 2, 'vowels: athugar hve margir sérljóðar úr íslenska stafrófinu eru í streng');
+console.assert(vowels("hallooo") === 4, 'vowels: athugar hve margir sérljóðar úr íslenska stafrófinu eru í streng');
+
+/**
+ * Fallið athugar hve margir samhljóðar úr íslenska stafrófinu eru í streng
+ * @param {string} str strengurinn sem á að finna fjölda samhljóða í
+ * @returns  fjöldi samhljóða í str
+ */
 function consonants(str) {
-  // Útfæra
+  let nmbr = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (CONSONANTS.includes(str[i])){
+      nmbr += 1;
+    }
+  }
+  return nmbr
 }
+
+
+console.assert(consonants("hallo") === 3, 'consonants: athugar hve margir sérljóðar úr íslenska stafrófinu eru í streng');
+console.assert(consonants("hallooo halloo") === 6, 'consonants: athugar hve margir sérljóðar úr íslenska stafrófinu eru í streng');
 
 //------------------------------------------------------------------------------
 // Leiðbeint ferli
 
 function start() {
-  // Útfæra
+  alert("Forrit þetta getur athugað hvað er lengsta orð og stysta orð í streng. Einnig skrifað út streng í öfugri röð og hvort hann sé palindrome. Þá getur forritið líka athugað hversu marga sam- og sérhljóða strengur inniheldur.");
+  
+  let input = prompt("Sláðu inn streng:");
+
+  if (input !== null && typeof input==='string') {
+
+    let longestWord = longest(input);
+    let shortestWord = shortest(input);
+    let reversedString = reverse(input);
+    let isPalindrome = palindrome(input);
+    let vowelCount = vowels(input);
+    let consonantCount = consonants(input);
+
+    alert(
+      `Niðurstöður fyrir strenginn   ${input}\n` +
+      `Lengsta orðið:   ${longestWord}\n` +
+      `Stysta orðið:   ${shortestWord}\n` +
+      `Er palindrome:   ${isPalindrome}\n` +
+      `Öfugt orð:   ${reversedString}\n` +
+      `Fjöldi sérhljóða:   ${vowelCount}\n` +
+      `Fjöldi samhljóða:   ${consonantCount}\n`
+    );
+
+    if (confirm("smelltu á ok til að byrja upp á nýtt eða smelltu á cancel til að hætta")) {
+      start()
+    }
+  }
 }
